8ec277ef8f9999afa866a08856be8c934f0eca66
chNajoua <najou.ch9@gmail.com>
2023-07-21 08:30:54 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - causes an infinite loop
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
@@ -13,7 +11,7 @@ int main(void)
 
 	i = 0;
 
-	/**
+	/*
 	 * while (i < 10)
 	 * {
 	 *	putchar(i);
